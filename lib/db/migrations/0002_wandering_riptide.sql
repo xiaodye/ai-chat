9f4f10ad4fcb9fa6cb05806df30cf936363c063c
lansen <bluewoods.lan@gmail.com>
2025-06-28 14:15:23 +0800

    fix: 修复 sql

--- lib/db/migrations/0002_wandering_riptide.sql
+++ lib/db/migrations/0002_wandering_riptide.sql
@@ -13,7 +13,11 @@ CREATE TABLE IF NOT EXISTS "Vote" (
 	CONSTRAINT "Vote_chatId_messageId_pk" PRIMARY KEY("chatId","messageId")
 );
 --> statement-breakpoint
-ALTER TABLE "Chat" ADD COLUMN "title" text NOT NULL;--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "Chat" ADD COLUMN "title" text NOT NULL;
+EXCEPTION
+ WHEN duplicate_column THEN null;
+END $$;--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "Message" ADD CONSTRAINT "Message_chatId_Chat_id_fk" FOREIGN KEY ("chatId") REFERENCES "public"."Chat"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
